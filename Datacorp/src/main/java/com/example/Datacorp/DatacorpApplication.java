7343bcb6e552d45acc2adbd0d8e43096aa96566d
Juan Diego <juandiegopossohidalgo@gmail.com>
2026-08-28 08:13:10 -0500

    se crea proyecto

---
+++ Datacorp/src/main/java/com/example/Datacorp/DatacorpApplication.java
@@ -0,0 +1,13 @@
+package com.example.Datacorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatacorpApplication.class, args);
+	}
+
+}
---
+++ Datacorp/src/test/java/com/example/Datacorp/DatacorpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Datacorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatacorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
